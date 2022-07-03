9d782350d4ca16fcb1ab0231f2a1dca1b1a8d948
Pratama Amirullisan <pratama.amirullisan@mst.co.id>
2022-07-04 00:17:09 +0700

    first commit

---
+++ js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function moveTo (id) {
+    document.getElementById(id).scrollIntoView();
+}
+
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cs-hidden');
+        } 
+    });  
+  });
